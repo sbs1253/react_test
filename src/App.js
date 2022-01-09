2f7dbfa46735fcd4f6b3e8e73e4b8d6ad636458e
sbs1253 <rokaf340@gmail.com>
2022-01-09 22:58:17 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useState } from 'react';
 function App() {
+  const [toDo, setToDo] = useState('');
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === '') {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo('');
+  };
   return (
-    <Router>
-      <Switch>
-        <Route path="/"></Route>
-        <Route></Route>
-      </Switch>
-    </Router>
+    <div>
+      <h1>My To Dos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Text"
+          onChange={onChange}
+          value={toDo}
+        />
+        <button>click</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
 export default App;
 //  내보낼때 중괄호 안써도됨
